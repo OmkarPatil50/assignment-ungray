b92157114020e038294be0952fa9eecc4ecdb502
OmkarPatil50 <omkarp5429@gmail.com>
2024-03-31 17:05:08 +0530

    feat(Component): :sparkles: Add Cards to display macrodata

---
+++ src/components/MacroData.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
+
+const MacroData = () => {
+  const data = [
+    {
+      topic: "Purchases",
+      amount: "4,294",
+      change: +32,
+    },
+    {
+      topic: "Revenue",
+      amount: "$322,3k",
+      change: +49,
+    },
+    {
+      topic: "Refunds",
+      amount: "$8,2k",
+      change: -7,
+    },
+  ];
+
+  return (
+    <div className="grid grid-cols-3 gap-5">
+      {data.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="border rounded-xl border-gray-300 shadow-sm p-3 space-y-2"
+          >
+            <p className="text-sm text-gray-400 font-medium">{item?.topic}</p>
+            <div className="flex justify-start items-center gap-2">
+              <p className="text-2xl font-bold">{item?.amount}</p>
+
+              <p
+                className={`px-2 py-1 rounded-full flex justify-start items-center gap-1 text-xs font-semibold ${
+                  item.change > 0
+                    ? "bg-[#00ff4f40] text-green-600 border border-green-600"
+                    : "bg-[#ff01014d] text-red-700 border border-red-700"
+                }`}
+              >
+                {item?.change}%{" "}
+                <span>
+                  {item?.change > 0 ? <HiTrendingUp /> : <HiTrendingDown />}
+                </span>
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default MacroData;
